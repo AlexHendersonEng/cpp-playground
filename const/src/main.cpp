26a0191bc204ee971c6aef8f1ec8f72ae8fa4c60
Alex Henderson <alexhendersnao@gmail.com>
2024-12-23 19:01:35 +0000

    Added const project

---
+++ const/src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Entity {
+private:
+	int m_X, m_Y;
+public:
+	int GetX() const // The const keyword stops any member variables being 
+		             // modified in this member function
+	{
+		return m_X;
+	}
+};
+
+int main() {
+	// Define const variable that will not be changed it is 'constant'
+	const int a = 20;
+	std::cout << "const variable: " << a << std::endl;
+
+	// Syntax difference between const pointer and const variable
+	int b = 20;
+	const int* c = &b; // Data being stored at the pointers memory address
+					   // is const
+	int* const d = &b; // The pointer value is const
+	const int* const e = &b; // Both the data being pointed to and pointer
+						     // value are const
+	std::cout << "Data at pointer address is const: " << *c << std::endl;
+	std::cout << "Pointer address is const: " << *d << std::endl;
+	std::cout << "Data and pointer address is const: " << *e << std::endl;
+
+	// Instantiate entity object and get x variable
+	Entity entity = Entity();
+	std::cout << "Entity class GetX method cannot modify class: "
+		      << entity.GetX() << std::endl;
+
+	// Wait for user input to stop debug window closing immediately
+	std::cin.get();
+}
