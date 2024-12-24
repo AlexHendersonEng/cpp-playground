b4db51910cd440c22f15be403c6fbe4350377778
Alex Henderson <alexhendersnao@gmail.com>
2024-12-24 14:24:18 +0000

    Added static function variable to static project

--- static/src/main.cpp
+++ static/src/main.cpp
@@ -21,9 +21,16 @@ struct Entity {
 int Entity::x;
 int Entity::y;
 
+// Create function with static variable
+void Count() {
+	static int count = 0; // static variable is persistent through calls to function
+	count++;
+	std::cout << "Count: " << count << std::endl;
+}
+
 int main() {
 	// Print out variable (only works if no static keyword in static.cpp)
-	std::cout << s_var << std::endl;
+	std::cout << "Static variables: " << std::endl << s_var << std::endl;
 
 	// Create two entities
 	Entity e1; e1.x = 1; e1.y = 2;
@@ -32,8 +39,15 @@ int main() {
 	// Print entities position (both are the same as entity 2 updates
 	// both entities x and y position as they are static member variables
 	// which are shared across all entity instances)
+	std::cout << "Static member variables and functions: " << std::endl;
 	Entity::Print(e1);
 	Entity::Print(e2);
+
+	// Call count function a few times
+	std::cout << "Static function variables: " << std::endl;
+	for (int i = 0; i < 5; i++) {
+		Count();
+	}
 
 	// Wait for user input to not close debug window instantly
 	std::cin.get();
